f47e9dc10abdb67c40705c0fbd511440922fa14c
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-10-30 04:11:35 +0700

    main

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+export default defineAppConfig({
+  ui: {
+    primary: "blue",
+    gray: "slate",
+    container: {
+      padding: "px-4 sm:px-10 lg:px-14",
+      constrained: "max-w-7xl",
+    },
+  },
+});
