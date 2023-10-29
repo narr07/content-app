92d2a51934b96d59366adaca3ba03d8d6ab84c92
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-10-30 03:40:50 +0700

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/ui", "@nuxtseo/module", "nuxt-og-image"],
+  site: {
+    url: process.env.NUXT_PUBLIC_SITE_URL || "http://localhost:3000/",
+    name: "Test Website",
+    description: "Welcome to my awesome site!",
+    defaultLocale: "id",
+  },
+});
---
+++ pages/[...slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <ContentDoc />
+  </main>
+</template>
+<script setup>
+  const route = useRoute();
+  const page = await queryContent(route.path).findOne();
+  useSeoMeta({
+    title: page.title,
+    ogTitle: page.title,
+    description: page.description,
+    ogDescription: page.description,
+    twitterDescription: page.description,
+    twitterTitle: page.title,
+  });
+  defineOgImage({
+    component: "Example",
+    title: page.title,
+    description: page.description,
+  });
+</script>
